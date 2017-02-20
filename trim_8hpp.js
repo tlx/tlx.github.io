70228b8d7d8cdbefc806d659dd82012252aaf118
mk-doxygen (Travis CI) <tlx@panthema.net>
2017-02-20 17:18:54 +0000

    Deploy code docs to GitHub Pages Travis build: 19
    Commit: 5ed67b8e553730eaf41f2e8c83431dc785b208bd

--- trim_8hpp.js
+++ trim_8hpp.js
@@ -2,14 +2,14 @@ var trim_8hpp =
 [
     [ "trim", "trim_8hpp.html#afc70026a4a1d57a74c7125f959245719", null ],
     [ "trim", "trim_8hpp.html#a3d1eecb0fadc2a1fddf598c7263ea4a9", null ],
-    [ "trim_copy", "trim_8hpp.html#a27485b91b71426bf179fc93dc6638caa", null ],
-    [ "trim_copy", "trim_8hpp.html#aa82fea497cc10c03c466076a2194e0a1", null ],
+    [ "trim", "trim_8hpp.html#a793ce30c7b3cff3a6111b41b07e883e1", null ],
+    [ "trim", "trim_8hpp.html#a32962b0cd33bfbad1b8b9f84d71171fc", null ],
     [ "trim_left", "trim_8hpp.html#aa95f062055820fcae0be3c5731217989", null ],
     [ "trim_left", "trim_8hpp.html#ad2e152d7231dc17cf6bbd32cb0c4fa21", null ],
-    [ "trim_left_copy", "trim_8hpp.html#af162732e350f2283a44a57b55437e707", null ],
-    [ "trim_left_copy", "trim_8hpp.html#ad5c76019a58838bfeab00f36932db4e0", null ],
+    [ "trim_left", "trim_8hpp.html#a51bb404a33a2f6f95c27a3ee392bc5d2", null ],
+    [ "trim_left", "trim_8hpp.html#ac21eb3ad7a12fc40ddf705915556656c", null ],
     [ "trim_right", "trim_8hpp.html#aa6ce44f04459898e5b6ca63ae5628c1f", null ],
     [ "trim_right", "trim_8hpp.html#a36e285a6834d509909818f1c12303993", null ],
-    [ "trim_right_copy", "trim_8hpp.html#ae900a3a492d3a21e07d3c75968b91d99", null ],
-    [ "trim_right_copy", "trim_8hpp.html#aabe2789ae52bf8db823326f1d796268f", null ]
+    [ "trim_right", "trim_8hpp.html#a9db34075fd2fa8291d5784450c3ff7ef", null ],
+    [ "trim_right", "trim_8hpp.html#a67f5099884188aee9526e5f7a9aefc4a", null ]
 ];
