e04df97ad96a59b42f40acad8eb8515eec340aa5
mk-doxygen (Travis CI) <tlx@panthema.net>
2019-12-12 15:29:38 +0000

    Deploy code docs to GitHub Pages Travis build: 339
    Commit: 903b9b35df8731496a90d8d74f8bedbad2517d9b

--- search/defines_4.js
+++ search/defines_4.js
@@ -27,6 +27,7 @@ var searchData=
   ['tlx_5fmerge3case',['TLX_MERGE3CASE',['../multiway__merge_8hpp.html#aa409c9d4294e037134a0c1f3937f9519',1,'multiway_merge.hpp']]],
   ['tlx_5fmerge4case',['TLX_MERGE4CASE',['../multiway__merge_8hpp.html#a7c16456e6331c298e313b1300bbcb69b',1,'multiway_merge.hpp']]],
   ['tlx_5fminor_5fversion',['TLX_MINOR_VERSION',['../version_8hpp.html#a63335ba7efedc082989fe21e89c57642',1,'version.hpp']]],
+  ['tlx_5fpatch_5fversion',['TLX_PATCH_VERSION',['../version_8hpp.html#ab1c0f53b44ed4f47e6a111304efe5a4b',1,'version.hpp']]],
   ['tlx_5fpos',['TLX_POS',['../multiway__merge_8hpp.html#aaed922dd2b60924d07d50c0251159ad7',1,'multiway_merge.hpp']]],
   ['tlx_5fsipcompress',['TLX_SIPCOMPRESS',['../siphash_8hpp.html#acdaafa9d242d6164e81b60fa64617feb',1,'siphash.hpp']]],
   ['tlx_5fslog',['TLX_sLOG',['../logger_2core_8hpp.html#a355617b6692391d5db2076a4fb46a818',1,'core.hpp']]],
--- version_8hpp.js
+++ version_8hpp.js
@@ -2,5 +2,6 @@ var version_8hpp =
 [
     [ "TLX_MAJOR_VERSION", "version_8hpp.html#a426c2a286896a9443b1e6936cfe071de", null ],
     [ "TLX_MINOR_VERSION", "version_8hpp.html#a63335ba7efedc082989fe21e89c57642", null ],
+    [ "TLX_PATCH_VERSION", "version_8hpp.html#ab1c0f53b44ed4f47e6a111304efe5a4b", null ],
     [ "TLX_VERSION", "version_8hpp.html#a94153792f475982e5e9f90098c466a39", null ]
 ];
